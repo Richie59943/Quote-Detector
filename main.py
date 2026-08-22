4aeaaba87a18f3e629cec779860940b909c6f6b0
Ricardo Ramirez <rich59943@gmail.com>
2026-08-22 07:26:29 -0700

    Updated main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#main is going to continuously run our other programs so we dont have to keep running our gmail.py file manually
+
+import time 
+
+from gmail_client import print_recent_emails
+
+CHECK_INTERVAL = 3600 #time check is every hour 
+
+def main():
+    print("Quote Email Monitor Started.")
+    print(f"Checking Gmail every hour")
+
+    while True: # a while loop going on forever 
+        try:
+            print("Checking for new emails..")
+
+            print_recent_emails()
+
+        except Exception as error:
+            print(f"Monitor error: {error}")
+
+        time.sleep(CHECK_INTERVAL)
+
+if __name__ == "__main__"
+    main()
